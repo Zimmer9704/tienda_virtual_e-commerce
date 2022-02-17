18e89e76a013902a33b1381080e86dd74e4f72be
Zimmer9704 <A-pr9704@hotmail.com>
2022-02-17 12:36:10 -0600

    Proyecto Online

---
+++ includes/brands-slider.php
@@ -0,0 +1,32 @@
+<div id="brands-carousel" class="logo-slider wow fadeInUp">
+		<h3 class="section-title">Marcas Reconocidas</h3>
+		<div class="logo-slider-inner">	
+			<div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/logo_Purina.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+				<br>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/cargill logo.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+				<br>
+
+				<div class="item">
+					<a href="#" class="image">
+						<img data-echo="brandsimage/agromas.png" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+
+
+
+		    </div><!-- /.owl-carousel #logo-slider -->
+	</div><!-- /.logo-slider-inner -->
+	
+</div>
+<!-- /.logo-slider -->
---
+++ includes/footer.php
@@ -0,0 +1,103 @@
+<footer id="footer" class="footer color-bg">
+	  <div class="links-social inner-top-sm">
+        <div class="container">
+            <div class="row">
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 <!-- ============================================================= CONTACT INFO ============================================================= -->
+<div class="contact-info">
+    <div class="footer-logo">
+        <div class="logo">
+            <a href="index.php">
+                
+<h3>Forrajera de Los Llanos Online</h3>
+            </a>
+        </div><!-- /.logo -->
+    
+    </div><!-- /.footer-logo -->
+
+     <div class="module-body m-t-20">
+        <p class="about-us"> Forrajera de Los Llanos Online.</p>
+    
+       
+
+        </div><!-- /.social-icons -->
+    </div><!-- /.module-body -->
+
+</div><!-- /.contact-info -->
+<!-- ============================================================= CONTACT INFO : END ============================================================= -->            	</div><!-- /.col -->
+
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 <!-- ============================================================= CONTACT TIMING============================================================= -->
+<div class="contact-timing">
+	<div class="module-heading">
+		<h4 class="module-title">Horario de atención</h4>
+	</div><!-- /.module-heading -->
+
+	<div class="module-body outer-top-xs">
+		<div class="table-responsive">
+			<table class="table">
+				<tbody>
+					<tr><td>Lunes-Sabado:</td><td class="pull-right">09:30 am a 2:30 pm</td></tr>
+                    <tr><td>Lunes-Sabado:</td><td class="pull-right">04:00 pm a 7:30 pm</td></tr>
+					<tr><td>Domingos:</td><td class="pull-right">Cerrado</td></tr>
+				</tbody>
+			</table>
+		</div><!-- /.table-responsive -->
+	</div><!-- /.module-body -->
+</div><!-- /.contact-timing -->
+<!-- ============================================================= CONTACT TIMING : END ============================================================= -->            	</div><!-- /.col -->
+
+            	<div class="col-xs-12 col-sm-6 col-md-4">
+            		 <!-- ============================================================= INFORMATION============================================================= -->
+<div class="contact-information">
+	<div class="module-heading">
+		<h4 class="module-title">Información</h4>
+	</div><!-- /.module-heading -->
+
+	<div class="module-body outer-top-xs">
+        <ul class="toggle-footer" style="">
+            <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-map-marker fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <p>Guadalupe Victoria, Durango, Mexico</p>
+                </div>
+            </li>
+
+              <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-mobile fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <p>(+51) 6768820316<br</p>
+                </div>
+            </li>
+
+              <li class="media">
+                <div class="pull-left">
+                     <span class="icon fa-stack fa-lg">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
+                    </span>
+                </div>
+                <div class="media-body">
+                    <span><a href="#">juanretana50@gmail.com</a></span>
+                </div>
+            </li>
+              
+            </ul>
+    </div><!-- /.module-body -->
+</div><!-- /.contact-timing -->
+<!-- ============================================================= INFORMATION : END ============================================================= -->            	</div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container -->
+    </div><!-- /.links-social -->
+
+    
